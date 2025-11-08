683ca308331d0ce05f712a12a6443404f84b292e
ManuYuzu <manuaglez@gmail.com>
2025-11-08 02:19:59 +0000

    chore: comment out unused imports and methods in AppController for cleaner code

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,24 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+// import { Controller, Get } from '@nestjs/common';
+// import { PrismaClient } from '@prisma/client';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-  private prisma = new PrismaClient();
+  // private prisma = new PrismaClient();
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
 
-  @Get('db-check')
-  async dbCheck(): Promise<string> {
-    try {
-      await this.prisma.$queryRaw`SELECT 1`;
-      return 'OK';
-    } catch (_e) {
-      return 'ERROR';
-    }
-  }
+  // @Get('db-check')
+  // async dbCheck(): Promise<string> {
+  //   try {
+  //     await this.prisma.$queryRaw`SELECT 1`;
+  //     return 'OK';
+  //   } catch (_e) {
+  //     return 'ERROR';
+  //   }
+  // }
 }
