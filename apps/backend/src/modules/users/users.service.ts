35106789ab8e34b939f356553b7785e7af59f851
ManuYuzu <manuaglez@gmail.com>
2025-11-09 22:39:14 +0000

    Basic CRUD for user implemented

---
+++ apps/backend/src/modules/users/users.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient, User } from '@prisma/client';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  private prisma = new PrismaClient();
+
+  async create(data: CreateUserDto): Promise<User> {
+    return this.prisma.user.create({
+      data: { ...data, role: data.role ?? 'READER' },
+    });
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.prisma.user.findMany();
+  }
+
+  async findOne(id: number): Promise<User | null> {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+
+  async update(id: number, data: UpdateUserDto): Promise<User> {
+    return this.prisma.user.update({ where: { id }, data });
+  }
+
+  async remove(id: number): Promise<User> {
+    return this.prisma.user.delete({ where: { id } });
+  }
+}
